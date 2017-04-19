32ad9a6d24950145232804b560ac59d874a6d7b7
Rahman Azhar <rahmanprojectd@gmail.com>
2017-04-19 11:22:34 +0800

    fix span

--- ImageSlider.js
+++ ImageSlider.js
@@ -190,3 +190,18 @@ export default class ImageSlider extends Component {
         </View>);
     }
 }
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
